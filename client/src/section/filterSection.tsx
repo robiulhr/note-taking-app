fd448a20a69d3ba69f49ae42e1d12c1c845e1ad9
robiulhr <rh22012001@gmail.com>
2023-12-24 00:51:37 +0600

    client: started adding the date range picker on the home page

--- client/src/section/filterSection.tsx
+++ client/src/section/filterSection.tsx
@@ -1,11 +1,25 @@
 import { Box, Paper } from "@mui/material";
 import TagFilter from "../component/tagFilter";
 import SearchForm from "../component/search";
+import { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
 export default function FilterSection() {
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(null);
+  const onChange = (dates) => {
+    const [start, end] = dates;
+    setStartDate(start);
+    setEndDate(end);
+  };
   return (
     <Box className="flex items-center justify-between my-7">
       <Paper elevation={1} className="h-[55px]">
         <SearchForm className="flex items-center justify-between h-[100%] w-[170px] sm:w-[200px] md:w-[250px] lg:w-[370px]" />
+      </Paper>
+      <Paper elevation={1}>
+        <DatePicker showIcon closeOnScroll={(e) => e.target === document} isClearable placeholderText="Select date or date range" showMonthDropdown showYearDropdown selected={startDate} onChange={onChange} startDate={startDate} endDate={endDate} selectsRange />
       </Paper>
       <Paper elevation={1}>
         <TagFilter className="w-[170px] sm:w-[200px] md:w-[250px] lg:w-[370px]" />
